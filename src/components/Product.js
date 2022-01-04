49a429297676b587e073ef5c190cbce77125f2b6
Susan Wairimu <mwihaki2susan@gmail.com>
2022-01-04 21:33:49 +0300

    handle product quantity increment and decrement

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import "../App.css";
 
 export default class Product extends Component {
+  state = {
+    product: this.props.product,
+  };
+
   render() {
     return (
       <div className=" col-lg-3">
@@ -9,29 +13,43 @@ export default class Product extends Component {
           <img
             className="card-img-top"
             style={{ height: "200px" }}
-            src={this.props.image}
+            src={this.state.product.image}
             alt=""
           />
           <div className="card-body">
             <h4 className="card-title">{this.props.productName}</h4>
             <h6 className="card-subtitle mt-2 mb-2 text-muted">
-              ID: {this.props.id}
+              ID: {this.state.product.id}
             </h6>
             <h5 className="card-subtitle mb-2 text-muted">
-              Ksh: {this.props.price}
+              Ksh: {this.state.product.price}
             </h5>
-            <p className="card-text">{this.props.description}</p>
+            <p className="card-text">{this.state.product.description}</p>
           </div>
           {/**card body ends here */}
 
           <div className="card-footer">
             <div className="float-start">
               <span className="badge badge-secondary bg-black m-2">
-                {this.props.quantity}
+                {this.state.product.quantity}
               </span>
               <div className="btn-group">
-                <button className="btn btn-outline-success m-2">+</button>
-                <button className="btn btn-outline-success m-2">-</button>
+                <button
+                  className="btn btn-outline-success m-2"
+                  onClick={() => {
+                    this.props.onIncrement(this.state.product, 10);
+                  }}
+                >
+                  +
+                </button>
+                <button
+                  className="btn btn-outline-success m-2"
+                  onClick={() => {
+                    this.props.onDecrement(this.state.product, 0);
+                  }}
+                >
+                  -
+                </button>
               </div>
             </div>
             <div className="float-end">{this.props.children}</div>
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -8,7 +8,7 @@ export default class ShoppingCart extends Component {
         id: Math.floor(Math.random() * 10000),
         productName: "iPhone",
         price: 35000,
-        quatity: 0,
+        quantity: 0,
         description: "Good",
         image:
           "https://images.unsplash.com/photo-1603891128711-11b4b03bb138?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aXBob25lJTIwMTF8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60",
@@ -17,7 +17,7 @@ export default class ShoppingCart extends Component {
         id: Math.floor(Math.random() * 10000),
         productName: "Samsung S21",
         price: 105000,
-        quatity: 0,
+        quantity: 0,
         description: "Good",
         image:
           "https://images.unsplash.com/photo-1610945415295-d9bbf067e59c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2Ftc3VuZyUyMHMyMXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60",
@@ -26,7 +26,7 @@ export default class ShoppingCart extends Component {
         id: Math.floor(Math.random() * 10000),
         productName: "Headphones",
         price: 1500,
-        quatity: 0,
+        quantity: 0,
         description: "Good",
         image:
           "https://media.istockphoto.com/photos/pink-headphones-picture-id1289318271?b=1&k=20&m=1289318271&s=170667a&w=0&h=02HeJjH6cjikL0SIthX_SHVAZuxMhzTEWBNSQ0sBWTA=",
@@ -35,7 +35,7 @@ export default class ShoppingCart extends Component {
         id: Math.floor(Math.random() * 10000),
         productName: "HP Laptop",
         price: 21000,
-        quatity: 0,
+        quantity: 0,
         description: "Good",
         image:
           "https://media.istockphoto.com/photos/laptop-picture-id90616136?b=1&k=20&m=90616136&s=170667a&w=0&h=3C88O8eBepFiROPkqm6yPvcjjANjLnwBP3hu3JuQ3k8=",
@@ -44,7 +44,7 @@ export default class ShoppingCart extends Component {
         id: Math.floor(Math.random() * 10000),
         productName: "PoweBank",
         price: 200,
-        quatity: 0,
+        quantity: 0,
         description: "Good",
         image:
           "https://images.unsplash.com/photo-1586253634019-c77872f966f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cG93ZXJiYW5rfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60",
@@ -53,7 +53,7 @@ export default class ShoppingCart extends Component {
         id: Math.floor(Math.random() * 10000),
         productName: "Microwave",
         price: 9500,
-        quatity: 0,
+        quantity: 0,
         description: "Good",
         image:
           "https://media.istockphoto.com/photos/modern-microwave-oven-picture-id139406154?b=1&k=20&m=139406154&s=170667a&w=0&h=Y3lNEt5oSTvQvbwijoFIra6fEhPhntT5fdReVHwxSJs=",
@@ -62,12 +62,40 @@ export default class ShoppingCart extends Component {
         id: Math.floor(Math.random() * 10000),
         productName: "Mini Bluetooth Speaker",
         price: 1500,
-        quatity: 0,
+        quantity: 0,
         description: "Good",
         image:
           "https://media.istockphoto.com/photos/portable-bluetooth-speaker-picture-id1129572489?b=1&k=20&m=1129572489&s=170667a&w=0&h=XYgU6HGlbISnLfUAALq26Z94nYjZm04F2i1Gnz0xcLw=",
       },
     ],
+  };
+
+  handeIncrement = (product, maxValue) => {
+    //get index of the selected product
+    let allProducts = [...this.state.products];
+    let index = allProducts.indexOf(product);
+
+    if (allProducts[index].quantity < maxValue) {
+      allProducts[index].quantity++;
+
+      this.setState({
+        products: allProducts,
+      });
+    }
+  };
+
+  handleDecrement = (product, minValue) => {
+    //get index of the selected product
+    let allProducts = [...this.state.products];
+    let index = allProducts.indexOf(product);
+
+    if (allProducts[index].quantity > minValue) {
+      allProducts[index].quantity--;
+
+      this.setState({
+        products: allProducts,
+      });
+    }
   };
 
   render() {
@@ -80,12 +108,9 @@ export default class ShoppingCart extends Component {
             return (
               <Product
                 key={prod.id}
-                id={prod.id}
-                productName={prod.productName}
-                price={prod.price}
-                quantity={prod.quatity}
-                image={prod.image}
-                description={prod.description}
+                product={prod}
+                onIncrement={this.handeIncrement}
+                onDecrement={this.handleDecrement}
               >
                 <button className="btn btn-primary">Buy Now</button>
               </Product>
