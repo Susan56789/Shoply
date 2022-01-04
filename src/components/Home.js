2277f1fc8bb2786a12def82bef605b6bc95e5203
Susan Wairimu <mwihaki2susan@gmail.com>
2022-01-04 11:02:45 +0300

    nested properties

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,12 +5,42 @@ export default class Home extends Component {
     pageTitle: "Customers",
     customersCount: 5,
     customers: [
-      { id: 1, name: "Jane Mumbi", phone: "0709775241" },
-      { id: 2, name: "John Kamau", phone: "0729435241" },
-      { id: 3, name: "Alex Opiyo", phone: "0791215241" },
-      { id: 4, name: "Mary Atieno", phone: "0721235241" },
-      { id: 5, name: "Mark Wafula", phone: "0732665241" },
-      { id: 6, name: "Stella Mwanza", phone: "0742655241" },
+      {
+        id: 1,
+        name: "Jane Mumbi",
+        phone: "0709775241",
+        address: { city: "Mombasa", county: "Mombasa" },
+      },
+      {
+        id: 2,
+        name: "John Kamau",
+        phone: "0729435241",
+        address: { city: "Nakuru", county: "Nakuru" },
+      },
+      {
+        id: 3,
+        name: "Alex Opiyo",
+        phone: "0791215241",
+        address: { city: "Nairobi", county: "Nairobi" },
+      },
+      {
+        id: 4,
+        name: "Mary Atieno",
+        phone: "0721235241",
+        address: { city: "Kisumu", county: "Kisumu" },
+      },
+      {
+        id: 5,
+        name: "Mark Wafula",
+        phone: "0732665241",
+        address: { city: "Bungoma", county: "Bungoma" },
+      },
+      {
+        id: 6,
+        name: "Stella Mwanza",
+        phone: "0742655241",
+        address: { city: "Nanyuki", county: "Laikipia" },
+      },
     ],
   };
 
@@ -41,15 +71,19 @@ export default class Home extends Component {
               <th>#</th>
               <th>Customer Name</th>
               <th>Phone</th>
+              <th>City</th>
+              <th>County</th>
             </tr>
           </thead>
           <tbody>
             {this.state.customers.map((cust) => {
               return (
-                <tr>
+                <tr key={cust.id}>
                   <td>{cust.id}</td>
                   <td>{cust.name}</td>
                   <td>{cust.phone}</td>
+                  <td>{cust.address.city}</td>
+                  <td>{cust.address.county}</td>
                 </tr>
               );
             })}
