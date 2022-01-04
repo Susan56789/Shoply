fd3f0e1a53c0431c472c385faaf8759567566b65
Susan Wairimu <mwihaki2susan@gmail.com>
2022-01-04 09:28:02 +0300

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+  state = {
+    appTitle: "Customers",
+  };
+
+  render() {
+    return (
+      <div>
+        <h4 className="border-bottom m-1 p-1">{this.state.appTitle}</h4>
+      </div>
+    );
+  }
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import logo from "../Logo .png";
+import "../App.css";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a className="navbar-brand" href="/">
+            <img src={logo} alt="shopify logo" className="logo-img" />
+            Shoply
+          </a>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <a className="nav-link" href="/#">
+                  Home
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </>
+    );
+  }
+}
