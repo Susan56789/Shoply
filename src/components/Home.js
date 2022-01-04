714be94d35847a7af143379005d3a2200dcb14b5
Susan Wairimu <mwihaki2susan@gmail.com>
2022-01-04 09:41:00 +0300

    customer Count state

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,18 @@ import React, { Component } from "react";
 export default class Home extends Component {
   state = {
     pageTitle: "Customers",
+    customersCount: 5,
   };
 
   render() {
     return (
       <div>
-        <h4 className="border-bottom m-1 p-1">{this.state.pageTitle}</h4>
+        <h4 className="border-bottom m-1 p-1 ">
+          {this.state.pageTitle}
+          <span className="badge badge-secondary m-2 bg-black">
+            {this.state.customersCount}
+          </span>
+        </h4>
       </div>
     );
   }
