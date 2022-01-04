257e0f9ea22e44ac1407b9eacb501d6351f8227a
Susan Wairimu <mwihaki2susan@gmail.com>
2022-01-04 19:42:39 +0300

    bootstrap class styling

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,8 +5,13 @@ export default class Product extends Component {
   render() {
     return (
       <div className=" col-lg-3">
-        <div className="card m-2" style={{ width: "18rem" }}>
-          <img className="card-img-top" src={this.props.image} alt="" />
+        <div className="card m-2" style={{ width: "20rem" }}>
+          <img
+            className="card-img-top"
+            style={{ height: "200px" }}
+            src={this.props.image}
+            alt=""
+          />
           <div className="card-body">
             <h4 className="card-title">{this.props.productName}</h4>
             <h6 className="card-subtitle mt-2 mb-2 text-muted">
@@ -15,12 +20,22 @@ export default class Product extends Component {
             <h5 className="card-subtitle mb-2 text-muted">
               Ksh: {this.props.price}
             </h5>
-            <p className="card-text text-muted">
-              Available: {this.props.quantity}
-            </p>
             <p className="card-text">{this.props.description}</p>
           </div>
-          <div className="card-footer text-left">{this.props.children}</div>
+          {/**card body ends here */}
+
+          <div className="card-footer">
+            <div className="float-start">
+              <span className="badge badge-secondary bg-black m-2">
+                {this.props.quantity}
+              </span>
+              <div className="btn-group">
+                <button className="btn btn-outline-success m-2">+</button>
+                <button className="btn btn-outline-success m-2">-</button>
+              </div>
+            </div>
+            <div className="float-end">{this.props.children}</div>
+          </div>
         </div>
       </div>
     );
